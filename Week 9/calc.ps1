8a50e3015c15b2d995838021f776f60b8cca4a9b
IronMansDaughter <90567442+IronMansDaughter@users.noreply.github.com>
2023-11-01 09:31:08 -0400

    Commit calc.ps1

--- Week 9/calc.ps1
+++ Week 9/calc.ps1
@@ -2,5 +2,4 @@
 
 Start-Process calc.exe
 Start-Sleep -Seconds 3
-# I kept getting an error here, but everything online says this is correct - I tried to also change the name of calculator name, I tried stop-service, and I even tried to get the id number for the calculator's process id
-Stop-Process -Name calc
+Stop-Process -Name win32calc
