8d0a6782bcd772f549f2a1a5c1d96b03392d659a
IronMansDaughter <90567442+IronMansDaughter@users.noreply.github.com>
2023-11-15 15:43:39 -0500

    Create IncidentResponseToolkit.ps1

--- Week 11/IncidentResponseToolkit.ps1
+++ Week 11/IncidentResponseToolkit.ps1
@@ -9,26 +9,26 @@ Q&A: Explain why you selected those four cmdlets and the value it would provide 
 
 
 
-# asks user what folder they want to say everything in or they can just hit enter and it'll save to the IncidentResponseToolKit
+# Asks user what folder they want to say everything in or they can just hit enter and it'll save to the IncidentResponseToolKit
 $folderPath = Read-Host "Enter the folder path to save the results or hit enter to save results to the IncidentReponseToolKit folder"
 New-Item -ItemType Directory -Path "$folderPath\IncidentResponseToolKit" | Out-Null
 New-Item -ItemType Directory -Path "$folderPath\IncidentResponseToolKit\csv" | Out-Null
 
 
 
-# gets running processes
+# Gets running processes
 Get-Process | Select-Object ProcessName, Path | Export-Csv -Path "$folderPath\IncidentResponseToolKit\csv\processes.csv" -NoTypeInformation
 Write-Host "Processes saved to $folderPath\IncidentResponseToolKit\csv\processes.csv"
 
 
 
-# gets registered services
+# Gets registered services
 Get-WmiObject -Class Win32_Service | Select-Object Name, DisplayName, PathName | Export-Csv -Path "$folderPath\IncidentResponseToolKit\csv\services.csv" -NoTypeInformation
 Write-Host "Services saved to $folderPath\IncidentResponseToolKit\csv\services.csv"
 
 
 
-# gets TCP network sockets
+# Gets TCP network sockets
 Get-NetTCPConnection | Select-Object LocalAddress, LocalPort, RemoteAddress, RemotePort, State | Export-Csv -Path "$folderPath\IncidentResponseToolKit\csv\tcpSockets.csv" -NoTypeInformation
 Write-Host "TCP sockets saved to $folderPath\IncidentResponseToolKit\csv\tcpSockets.csv"
 
@@ -40,37 +40,35 @@ Write-Host "User accounts saved to $folderPath\IncidentResponseToolKit\csv\users
 
 
 
-# gets network adapter config info
+# Gets network adapter config info
 Get-WmiObject -Class Win32_NetworkAdapterConfiguration -Filter IPEnabled=TRUE | Format-List IPAddress, DefaultIPGateway, DNSServerSearchOrder, DHCPServer | Export-Csv -Path "$folderPath\IncidentResponseToolKit\csv\networkAdapterConfiguration.csv" -NoTypeInformation
 Write-Host "Network adapter configs saved to $folderPath\IncidentResponseToolKit\csv\networkAdapterConfiguration.csv"
 
+# Below is the code for the 4 artifacts 
 
-
-# below is the code for the 4 artifacts 
-
-# saves system event logs(CSV)
+# Saves system event logs(CSV)
 Get-EventLog -LogName System -Newest 1000 | Export-Csv -Path "$folderPath\IncidentResponseToolKit\csv\sysEventLogs.csv" -NoTypeInformation
 Write-Host "System event logs saved to $folderPath\IncidentResponseToolKit\csv\sysEventLogs.csv"
 
 
 
-# saves security event logs(CSV)
+# Saves security event logs(CSV)
 Get-EventLog -LogName Security -Newest 1000 | Export-Csv -Path "$folderPath\IncidentResponseToolKit\csv\secEventLogs.csv" -NoTypeInformation
 Write-Host "Security event logs saved to $folderPath\IncidentResponseToolKit\csv\secEventLogs.csv"
 
 
 
-# saves application event logs(CSV)
+# Saves application event logs(CSV)
 Get-EventLog -LogName Application -Newest 1000 | Export-Csv -Path "$folderPath\IncidentResponseToolKit\csv\appEventLogs.csv" -NoTypeInformation
 Write-Host "Application event logs saved to $folderPath\IncidentResponseToolKit\csv\appEventLogs.csv"
 
 
 
-# saves PS history(CSV)
+# Saves PS(PowerShell) history(CSV)
 Get-WinEvent Microsoft-Windows-PowerShell/Operational -MaxEvents 1000 | Export-Csv -Path "$folderPath\IncidentResponseToolKit\csv\psEventLogs.csv" -NoTypeInformation
 Write-Host "PowerShell event logs saved to $folderPath\IncidentResponseToolKit\csv\psEventLogs.csv"
 
-# creates file hashes
+# Creates file hashes
 function create_hash_file($num) {
     
     if ($num -eq 1) {
@@ -85,12 +83,12 @@ function create_hash_file($num) {
     }
 }
 
-# takes the file hashes for each CSV file and puts them into a new file:
+# Takes the file hashes for each CSV file and puts them into a new file:
 create_hash_file(1)
 
-#zips results
+#Zips results
 $zipPath = "$folderPath\IncidentResponseToolKit\results.zip"
 Compress-Archive -Path "$folderPath\IncidentResponseToolKit\csv\*" -DestinationPath $zipPath
 
-# gets the hash for the zip file and save it to a new file
+# Gets the hash for the zip file and save it to a new file
 create_hash_file(2)
